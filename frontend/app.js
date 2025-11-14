19ef0d554614d3ed786e72e474514da9d007110a
John_Orina <Johnorina21@gmail.com>
2025-11-14 15:57:52 -0500

    more front end work

---
+++ frontend/app.js
@@ -0,0 +1,11 @@
+function goToCreateList(){
+    window.location.href="create-list.html";
+}
+
+let task = []; 
+function addTask() {
+    const input = document.getElementById("taskInput");
+    const value = input.value.trim();
+
+    if (value == "") return; 
+}
