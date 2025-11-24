969054851509297307bbfe1ab29bffd8386d580f
John_Orina <Johnorina21@gmail.com>
2025-11-23 23:49:19 -0500

    added a posting tasks functionality

--- frontend/app.js
+++ frontend/app.js
@@ -9,3 +9,29 @@ function addTask() {
 
     if (value == "") return; 
 }
+
+
+function addTask(){
+    //getting input from the text box
+    const input = document.getElementById("taskInput");
+
+    //trimming spaces from the input
+    const taskText = input.value.trim();
+
+    if (taskText == ""){
+        alert('Please enter a task first');
+        return;
+    }
+
+    //create a new div element for the task
+    const taskItem = document.createElement("div");
+    taskItem.className = "task-item"; //class for css 
+    taskItem.textContent = taskText;
+
+    //append the new task to the taskList div
+    const list = document.getElementById("taskList");
+    list.appendChild(taskItem);
+
+    //clear the input
+    input.value = "";
+}
