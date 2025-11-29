e160d938f4486e6c4ed570648e7a6349f740b435
John_Orina <Johnorina21@gmail.com>
2025-11-28 22:29:23 -0500

    more file organization

--- server.config/config.js
+++ server.config/config.js
@@ -1,5 +1,14 @@
 //changing home page 
+const express = require("express");
+const path = require("path"); 
+const app = express();
+
 
 app.get("/", (req,res)=>{
-    res.sendFile(__dirname,frontend/home.html)
+    res.sendFile(path.join(__dirname,frontend/home.html));
 })
+
+app.get("/login", (req,res) => {
+    res.sendFile(path.join(__dirname, "../frontend/pages/login.html")); 
+})
+
